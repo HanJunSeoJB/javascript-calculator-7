e937c5062a5d657bdcbce6be3816b7aa7eb96d4c
HanJunSeoJB <hjs123@jbnu.ac.kr>
2024-10-15 20:47:50 +0900

    feat(input): 구분자 예외처리 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    
+    const input = await MissionUtils.Console.readLineAsync();
+    if (!input.includes(":") || !input.includes(",")) {
+      throw new Error("구분자가 올바르지 않습니다.");
+    }
+  }
 }
 
 export default App;
